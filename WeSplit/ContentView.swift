81b42fa5ef7f17dab639f76462a32b7882fa4cc5
ayrton <ayrton.parkinson1@gmail.com>
2024-07-11 07:18:46 -0400

    Add check for textfield focus and button to unfocus

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @State private var numberOfPeople = 0
     @State private var tipPercentage = 20
     
+    @FocusState private var amountIsFocused: Bool
+    
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
         let tipSelection = Double(tipPercentage)
@@ -29,6 +31,7 @@ struct ContentView: View {
                 Section {
                     TextField("Amount", value: $checkAmount, format: .currency(code: "USD"))
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
@@ -51,6 +54,13 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
