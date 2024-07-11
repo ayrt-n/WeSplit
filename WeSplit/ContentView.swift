b9981258718c6e8aea5a06b42ea8a14ad70bd5ac
ayrton <ayrton.parkinson1@gmail.com>
2024-07-11 05:56:13 -0400

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Ayrton Parkinson on 2024/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ayrton Parkinson on 2024/07/11.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
